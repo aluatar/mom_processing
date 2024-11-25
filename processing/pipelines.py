6af359a621b75eef2aded102d2316810f7fecd05
Fedor Shuklin <fedor.shuklin.95@mail.ru>
2024-11-25 18:33:53 +0300

    Added mew piplines

--- processing/pipelines.py
+++ processing/pipelines.py
@@ -53,3 +53,57 @@ def pipeline_modes_vs_h(path: str) -> dict[int, dict[str, array]]:
         }
         
     return mode_m_map
+
+
+def pipeline_modes_vs_r(path: str) -> dict[int, dict[str, array]]:
+    number_of_parameters = 1
+    M_0 = 140 * 1e3 #[A/m]
+    gamma = 2.21e5 #
+    giga = 1e9
+
+    df = import_data(path=path)
+    df = separate_characteristics_from_params(
+        dataframe=df,
+        number_of_params=number_of_parameters)
+    df = get_orbital_numbers(dataframe=df)
+    columns = df.columns
+
+    unique_ms = unique(df['m'])
+    mode_m_map = {}
+    for m in unique_ms:
+        r = array(df.loc[df['m'] == m][columns[0]])
+        freq = array(df.loc[df['m'] == m][columns[1]])
+
+        mode_m_map[m] = {
+            'freq': freq,
+            'r': r
+        }
+        
+    return mode_m_map
+
+
+def pipeline_modes_vs_s(path: str) -> dict[int, dict[str, array]]:
+    number_of_parameters = 1
+    M_0 = 140 * 1e3 #[A/m]
+    gamma = 2.21e5 #
+    giga = 1e9
+
+    df = import_data(path=path)
+    df = separate_characteristics_from_params(
+        dataframe=df,
+        number_of_params=number_of_parameters)
+    df = get_orbital_numbers(dataframe=df)
+    columns = df.columns
+
+    unique_ms = unique(df['m'])
+    mode_m_map = {}
+    for m in unique_ms:
+        r = array(df.loc[df['m'] == m][columns[0]])
+        freq = array(df.loc[df['m'] == m][columns[1]])
+
+        mode_m_map[m] = {
+            'freq': freq,
+            's': r
+        }
+        
+    return mode_m_map
